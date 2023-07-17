f3146b7a18d6ca5e454b76ae190cb2f70ed74136
boba <44716190+bobatealee@users.noreply.github.com>
2023-07-17 03:18:14 -0500

    2.0.0-RC1

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// game mode check
 const params = new Proxy(new URLSearchParams(window.location.search), {
 	get: (searchParams, prop) => searchParams.get(prop),
 });
@@ -5,15 +6,25 @@ var gameMode = params.game;
 
 document.addEventListener('DOMContentLoaded', (event) => {
 	// title outlines
-	$(function() {
-		$('.titleOutline').attr('letter', function() {return $(this).html();});
+	document.querySelectorAll(".textOutline").forEach((textOutline) => {
+		textOutline.setAttribute("outline", textOutline.innerHTML);
 	});
+
+	document.getElementById("dividerButton").addEventListener('click', dividerToggle);
 
 	if (gameMode == 1) {
 		document.body.classList.add("gameMode");
 	}
-
-	for (let i = 0; i < document.getElementsByClassName("desktopButton").length; i++) {
-		document.getElementsByClassName("desktopButton")[i].style.backgroundPosition = -(i*32)+"px 0px";
-	}
 });
+
+function dividerToggle() {
+	if (document.getElementById("sidebar").classList.contains("show")) {
+		document.getElementById("sidebar").classList.remove("show");
+		document.getElementById("dividerButton").innerHTML = "Links ▼";
+		document.getElementById("dividerButton").setAttribute("outline", document.getElementById("dividerButton").innerHTML);
+	} else {
+		document.getElementById("sidebar").classList.add("show");
+		document.getElementById("dividerButton").innerHTML = "Links ▲";
+		document.getElementById("dividerButton").setAttribute("outline", document.getElementById("dividerButton").innerHTML);
+	}
+}
