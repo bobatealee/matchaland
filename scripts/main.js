971589bd44b1e1c8bcf91ebedaf3011fd386161f
boba <44716190+bobatealee@users.noreply.github.com>
2023-07-18 19:35:39 -0500

    2.1.0. many fixes

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,6 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 var gameMode = params.game;
 
 document.addEventListener('DOMContentLoaded', (event) => {
-	// title outlines
-	document.querySelectorAll(".textOutline").forEach((textOutline) => {
-		textOutline.setAttribute("outline", textOutline.innerHTML);
-	});
-
 	document.getElementById("dividerButton").addEventListener('click', dividerToggle);
 
 	if (gameMode == 1) {
@@ -46,10 +41,8 @@ function dividerToggle() {
 	if (document.getElementById("sidebar").classList.contains("show")) {
 		document.getElementById("sidebar").classList.remove("show");
 		document.getElementById("dividerButton").innerHTML = "Links ▼";
-		document.getElementById("dividerButton").setAttribute("outline", document.getElementById("dividerButton").innerHTML);
 	} else {
 		document.getElementById("sidebar").classList.add("show");
 		document.getElementById("dividerButton").innerHTML = "Links ▲";
-		document.getElementById("dividerButton").setAttribute("outline", document.getElementById("dividerButton").innerHTML);
 	}
 }
