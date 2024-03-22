41cdf9feb17cfcf8a4358b6f7e4cdb37784cd318
John Q. Herman <johnqherman@gmail.com>
2024-03-21 21:16:35 -0400

    Restructure constants for better modularity & reusability

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,40 +1,44 @@
 const resourcesBaseUrl = "https://resources.matchaland.net";
 
-// game/map images or lack thereof
-export const serverGameSrc = `${resourcesBaseUrl}/games/${game}.png`;
-export const serverMapSrc = `${resourcesBaseUrl}/maps/${game}/${serverMapImg}`;
-export const unknownGameSrc = `${resourcesBaseUrl}/games/unknown.png`;
-export const unknownMapSrc = `${resourcesBaseUrl}/maps/unknown.png`;
-
-// online/offline status images
-export const onlineStatusSrc = `${resourcesBaseUrl}/status/online.png`;
-export const offlineStatusSrc = `${resourcesBaseUrl}/status/offline.png`;
-
-// raccoonlagoon api
-export const apiEndpoint = `https://api.raccoonlagoon.com/v1/server-info?ip=${ip}&g=${game}`
-
-// arithmetic constants
-export const SECONDS_IN_DAY = 24 * 60 * 60;
-export const SECONDS_IN_HOUR = 60 * 60;
-export const SECONDS_IN_MINUTE = 60;
-
-/* TODO: format this nicely and bring more stuff over from servers.js
-
-e.g:
+const BASE_URLS = {
+  games: `${resourcesBaseUrl}/games/`,
+  maps: `${resourcesBaseUrl}/maps/`,
+  status: `${resourcesBaseUrl}/status/`,
+};
 
 export const RESOURCES = {
-    BASE_URLS: {
-        games: `${resourcesBaseUrl}/games/`,
-        maps: `${resourcesBaseUrl}/maps/`,
-        status: `${resourcesBaseUrl}/status/`
-    },
-    IMAGE_SOURCES: {
-        game: (game) => `${BASE_URLS.games}${game}.png`,
-        unknownGame: `${BASE_URLS.games}unknown.png`,
-        map: (game, serverMap) => `${BASE_URLS.maps}${game}/${serverMap}.png`,
-        unknownMap: `${BASE_URLS.maps}unknown.png`,
-        onlineStatus: `${BASE_URLS.status}online.png`,
-        offlineStatus: `${BASE_URLS.status}offline.png`
-    },
-}
-*/
+  BASE_URLS: BASE_URLS,
+  IMAGE_SOURCES: {
+    serverGame: (game) => `${BASE_URLS.games}${game}.png`,
+    unknownGame: `${BASE_URLS.games}unknown.png`,
+    serverMap: (game, serverMap) => `${BASE_URLS.maps}${game}/${serverMap}.png`,
+    unknownMap: `${BASE_URLS.maps}unknown.png`,
+    onlineStatus: `${BASE_URLS.status}online.png`,
+    offlineStatus: `${BASE_URLS.status}offline.png`,
+  },
+  API_ENDPOINT: (ip, game) =>
+    `https://api.raccoonlagoon.com/v1/server-info?ip=${ip}&g=${game}`,
+};
+
+export const TIME_CONSTANTS = {
+  SECONDS_IN_DAY: 24 * 60 * 60,
+  SECONDS_IN_HOUR: 60 * 60,
+  SECONDS_IN_MINUTE: 60,
+};
+
+// abstaction = pain but luckily we're not
+// touching this stuff ever again Smiley Face
+export const SERVER_INFO = {
+  cleanServerName: (data, overrideName) =>
+    (overrideName ? overrideName : data.serverName).replace(/�./g, ""),
+
+  botsText: (data) => (data.numBots > 0 ? ` (${data.numBots} bots)` : ""),
+
+  playersText: (data) =>
+    `${data.numHumans}/${data.maxClients} ${SERVER_INFO.botsText(data)}`,
+
+  connectOrDynmap: (data, dynmap) =>
+    dynmap ? `http://${dynmap}` : `steam://connect/${data.serverIP}`,
+
+  serverButtonText: (dynmap) => (dynmap ? "Dynmap" : "Connect"),
+};
